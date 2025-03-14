eaf9140a231765624d90be46f8185f533390def7
juarezsanty <juarezsanty04@gmail.com>
2025-03-14 19:40:46 -0300

    junta de las preguntas con sus respuestas y opciones en una tupla para evitar acceso por indices

--- questions.py
+++ questions.py
@@ -6,15 +6,15 @@ questions = [ "¿que funcion se usa para obtener la longitud de una cadena de py
 answers = [ ("size()" , "len()" , "lenght()", "count()") , ("3.14" , "'42'" , "10", "true") , ("input()" , "scan()" , "read()" , "ask()") , ("//esto es un comentario" , "/* esto es un comentario */" , "--esto es un comentario" , "# esto es un comentario" ,) , ("=", "==", "!=", "===") , ]
 #indice de la respuesta correcta para cada pregunta, en el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+#junta 3 preguntas aleatorias con sus opciones y respuestas correctas en una tupla
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k = 3)
 #el usuario debe contestar 3 preguntas
 puntaje = 0.0
-for _ in range(3):
-	# se selecciona una pregunta aleatoria
-	question_index = random.randint(0, len(questions) -1)
+for question, answer_choices, correct_answers_index in questions_to_ask:
 	
 	#se muestra la pregunta y las respuestas posibles
-	print (questions[question_index])
-	for i, answer in enumerate(answers[question_index]):
+	print (question)
+	for i, answer in enumerate(answer_choices):
 		print (f"{i + 1}. {answer}")
 	
 	#el usuario tiene 2 intentos para responder correctamente
@@ -22,9 +22,9 @@ for _ in range(3):
 		user_answer = input("respuesta: ")
 		if user_answer.isdigit():
 			user_answer = int(user_answer) - 1
-			if user_answer >= 0 and user_answer < len(answers[question_index]):
+			if user_answer >= 0 and user_answer < len(answer_choices):
 				#se verifica si la respuesta es correcta
-				if user_answer == correct_answers_index[question_index]:
+				if user_answer == correct_answers_index:
 					print("¡correcto!")
 					puntaje += 1.0
 					break
@@ -39,7 +39,7 @@ for _ in range(3):
 	else:
 		#si el usuario no responde correctamente despues de 2 intentos se muestra la respuesta correcta
 		print ("incorrecto. la respuesta correcta es: ")
-		print(answers[question_index][correct_answers_index[question_index]])
+		print(answer_choices[correct_answers_index])
 	
 	#se imprime un blanco al final de la pregunta
 	print()
